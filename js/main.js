b656447faaf26aa9ce1b0fc893125ca861d2b6ad
Колесников Егор <koles.e.13@mail.ru>
2021-05-29 22:58:27 +0700

    Вносит доработки

--- js/main.js
+++ js/main.js
@@ -1,27 +1,20 @@
-function getRandomInteger (from, to) {
-  if (from >= 0) {
-    if (to <= from) {
-      return 'Некорректный диапазон';
-    }
+const getRandomInteger = (from, to) => {
+  if (from >= 0 && to > from) {
     return Math.floor(Math.random() * (to + 1 - from) + from);
     // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   }
-  return 'Диапазон должен быть положительным';
+  return 'Некорректный диапазон';
 }
 
 getRandomInteger(1,10);
 
-function getRandomFloat (from, to, point) {
-  if (from >= 0) {
-    if (to <= from) {
-      return 'Некорректный диапазон';
-    }
-    const result =  Math.random() * (to - from) + from;
-    // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    return result.toFixed(point);
-    // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+const getRandomFloat = (from, to, point) => {
+  if (from >= 0 && to > from) {
+    return (Math.random() * (to - from) + from).toFixed(point);
+    /* Источники: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random;
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed */
   }
-  return 'Диапазон должен быть положительным';
+  return 'Некорректный диапазон';
 }
 
 getRandomFloat(1,10,3);
